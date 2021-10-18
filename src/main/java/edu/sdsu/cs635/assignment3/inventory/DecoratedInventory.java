8890ab886a1574afe30f248dc2de42b69f84157b
Harsh Maheshwari <hrshdg8@gmail.com>
2021-10-17 23:14:59 -0700

    Move to correct path

--- src/main/java/edu/sdsu/cs635/assignment3/inventory/DecoratedInventory.java
+++ src/main/java/edu/sdsu/cs635/assignment3/inventory/DecoratedInventory.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public class DecoratedInventory implements Inventory {
   private Inventory inventory;
-  private WithSaveToFile invoker;
+  private final WithSaveToFile invoker;
 
   public DecoratedInventory(Inventory inventory) {
     this.inventory = inventory;
