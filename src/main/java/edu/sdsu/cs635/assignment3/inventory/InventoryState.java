72c9c6db8b1eb8e6672ca9b816d68d6618152b6e
Harsh Maheshwari <hrshdg8@gmail.com>
2021-10-20 16:56:11 -0700

    Decorator correction

--- src/main/java/edu/sdsu/cs635/assignment3/inventory/InventoryState.java
+++ src/main/java/edu/sdsu/cs635/assignment3/inventory/InventoryState.java
@@ -3,7 +3,7 @@ package edu.sdsu.cs635.assignment3.inventory;
 import java.io.Serializable;
 
 /**
- * holds a copy of the Inventory to be able to roll back to a previous stored version
+ * holds a deep copy of the {@link Inventory} to be able to roll back to a previous stored version
  */
 public class InventoryState implements Serializable {
   private static final long serialVersionUID = -4778810560316362990L;
